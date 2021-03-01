c01d2f834c7134695b8dc01254dd338bd26ec21f
Petarus <petarusart@gmail.com>
2021-03-01 14:42:56 -0500

    Sorting Order
    Added Scripts "ChildTrigger" and "NPC" to toggle sorting order of characters when walking above them.

---
+++ Assets/Scripts/ChildTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildTrigger : MonoBehaviour
+{
+    public string targetTag;
+
+    [Header("Enter")]
+    public string enterMessage;
+    public float enterValue;
+
+    [Header("Exit")]
+    public string exitMessage;
+    public float exitValue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == targetTag)
+        {
+            SendMessageUpwards(enterMessage, enterValue, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == targetTag)
+        {
+            SendMessageUpwards(exitMessage, exitValue, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    public void setOrder(int order){
+        sr.sortingOrder = order;
+    }
+}
