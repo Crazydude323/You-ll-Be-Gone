90cdfc1841f35cf37fe336d26679dbece4376386
Petarus <petarusart@gmail.com>
2021-03-01 12:46:08 -0500

    Hook Up MC Placeholder Animation
    Cleaned up Player script, Configured Animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
+    public float speed=1f;
+    private Vector2 movement;
 
-    public float speed=10f;
+    [Header("Components")]
     private Rigidbody2D r2d;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         r2d = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,10 +24,27 @@ public class Player : MonoBehaviour
     {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontal, vertical);
+        movement = new Vector2(horizontal, vertical);
         movement.Normalize();
+    }
 
-        r2d.velocity = movement * speed * Time.deltaTime;
-        
+    private void FixedUpdate()
+    {
+
+        //move sprite
+        r2d.velocity = movement * speed;
+
+        //flip player based on movement direction
+        if (movement.x < 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (movement.x > 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //update animator
+        animator.SetFloat("speed", Mathf.Abs(movement.magnitude));
     }
 }
